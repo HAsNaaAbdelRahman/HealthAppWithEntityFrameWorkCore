ab9bf9e5489e1e5965304c8bdba64f83f54fa58b
Hasnaa AbdelRahman <83743822+HAsNaaAbdelRahman@users.noreply.github.com>
2024-09-14 12:46:14 +0300

    Add files via upload

---
+++ CodeFirst/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using CodeFirst.Models;
+
+namespace CodeFirst
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlServer("Data Source=HASNAA-ABDELRAH;Initial Catalog=HealthAppv2;;Integrated Security=True;TrustServerCertificate=True");
+        // 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserHealth>().HasNoKey();
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet <UserHealth> UsersHealth { get; set; }
+        public DbSet <Authentication> Authentication { get; set; }
+        public DbSet<Sleep> Sleep { get; set; }
+        public DbSet<Daily> Daily { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Goal> Goals { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+
+
+
+    }
+}
