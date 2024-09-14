42774be9de86f4947c8ee381cdf1ba269bf3d7b8
Hasnaa AbdelRahman <83743822+HAsNaaAbdelRahman@users.noreply.github.com>
2024-09-14 12:53:57 +0300

    Add files via upload

---
+++ DataBaseFirst/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseFirst
+{
+    public class AplicationDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlServer("Server=HASNAA-ABDELRAH;Database=HealthApp;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true");
+
+    }
+}
